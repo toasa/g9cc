e32f7335c465f91c6881c50a56e8d9ca8981b650
toasa <atsushi.tohyama.160.333@gmail.com>
2018-11-12 20:09:58 +0900

    implement: block scope

--- examples/9queen.c
+++ examples/9queen.c
@@ -1,8 +1,105 @@
+#include <stdio.h>
+
+void printBoard(int board[100]) {
+  for (int i = 0; i < 9; i=i+1) {
+	for (int j = 0; j < 9; j=j+1) {
+	  if (board[i * 9 + j] == 0) {
+		printf(". ");
+	  } else {
+		printf("Q ");
+	  }
+	}
+	printf("\n");
+  }
+  printf("\n");
+}
+
+int isQ(int board[100], int row, int col) {
+  if (board[9*row + col] == 1) {
+	return 1;
+  } else {
+	return 0;
+  }
+}
+
+int conflict(int board[100], int row, int col) {
+  for (int i = 1; i < 9; i++) {
+	// check diagonal
+	if ((row-i) >= 0 && (col-i) >= 0) {
+	  if (isQ(board, row-i, col-i)) {
+		goto conf;
+	  }
+	}
+	if ((row-i) >= 0 && (col+i) < 9) {
+	  if (isQ(board, row-i, col+i)) {
+		goto conf;
+	  }
+	}
+	if ((row+i) < 9 && (col-i) >= 0) {
+	  if (isQ(board, row+i, col-i)) {
+		goto conf;
+	  }
+	}
+	if ((row+i) < 9 && (col+i) < 9) {
+	  if (isQ(board, row+i, col+i)) {
+		goto conf;
+	  }
+	}
+
+	for (int j = 1; j < 9; j++) {
+	  // check vertical
+	  if (row - i >= 0) {
+		if (isQ(board, row-i, col)) {
+		  goto conf;
+		}
+	  }
+	  if (row + i < 9) {
+		if (isQ(board, row+i, col)) {
+		  goto conf;
+		}
+	  }
+
+	  // check horizontal
+	  if ((col - j) >= 0) {
+		if (isQ(board, row, col-j)) {
+		  goto conf;
+		}
+	  }
+	  if ((col + j) < 9) {
+		if (isQ(board, row, col+j)) {
+		  goto conf;
+		}
+	  }
+	}
+  }
+  return 0;
+  
+ conf:
+  return 1;
+}
+
+
+void solve(int board[100], int row) {
+  if (row > 9) {
+	printBoard(board);
+	return;
+  }
+  for (int col = 0; col < 9; col++) {
+	if (conflict(board, row, col)) {
+	  continue;
+	} else {
+	  board[9*row + col] = 1;
+	  solve(board, row+1);
+	  board[9*row + col] = 0; // I forget this stmt ><
+	}
+  }
+}
+
 int main(){
-    int board[100];
-    for (int i = 0; i < 100; i=i+1) {
-        board[i] = 0;
-    }
-    solve(board, 0)
-    return 0
+  int board[100];
+  for (int i = 0; i < 100; i=i+1) {
+	board[i] = 0;
+  }
+  solve(board, 0);
+  return 0;
 }
--- sema/sema.go
+++ sema/sema.go
@@ -12,11 +12,38 @@ import (
 
 var int_ty Type = Type{Ty: INT, Ptr_of: nil, Ary_of: nil, Len: 0}
 
-var str_label int
-// 各識別子において、rbpからのoffsetを登録するための辞書
-var vars map[string]interface{}
+// Compound statement( {...; ...; ...;} )内に変数のスコープを制限するための構造体Env.
+// 各comp stmtごとに変数を登録するためのmap, varsを持っている
+type Env struct {
+    // 各識別子において、rbpからのoffsetを登録するための辞書
+    vars map[string]interface{}
+
+    next *Env
+}
+
 var globals *Vector
+var str_label int
 var stacksize int
+
+func new_env(next *Env) *Env{
+    env := new(Env)
+    env.vars = make(map[string]interface{})
+    env.next = next
+    return env
+}
+
+func find(env *Env, name string) *Var {
+    for ; env != nil; env = env.next {
+        if env.vars[name] != nil {
+            var_ := env.vars[name].(*Var)
+            if var_ != nil {
+                return var_
+            }
+        }
+
+    }
+    return nil
+}
 
 func swap(p **Node, q **Node) {
     var r *Node = *p
@@ -40,7 +67,7 @@ func addr_of(base *Node, ty *Type) *Node {
 }
 
 // ASTを渡り歩く
-func walk(node *Node, decay bool) *Node {
+func walk(env *Env, node *Node, decay bool) *Node {
     switch node.Op {
     case ND_NUM:
         return node
@@ -59,14 +86,14 @@ func walk(node *Node, decay bool) *Node {
         ret.Op = ND_GVAR
         ret.Ty = node.Ty
         ret.Name = var_.Name
-        return walk(ret, decay)
+        return walk(env, ret, decay)
     case ND_IDENT:
-        var_, ok := vars[node.Name].(*Var)
+        var_ := find(env, node.Name)
 
-        if !ok {
+        if var_ == nil {
             Error(fmt.Sprintf("undefined variable: %s", node.Name))
         }
-        //node.Ty = var_.ty
+
         node.Op = ND_LVAR
         node.Offset = var_.Offset
 
@@ -92,28 +119,28 @@ func walk(node *Node, decay bool) *Node {
         var_.Ty = node.Ty
         var_.Is_local = true
         var_.Offset = stacksize
-        vars[node.Name] = var_
+        env.vars[node.Name] = var_
 
         if node.Init != nil {
-            node.Init = walk(node.Init, true)
+            node.Init = walk(env, node.Init, true)
         }
         return node
     case ND_IF:
-        node.Cond = walk(node.Cond, true)
-        node.Then = walk(node.Then, true)
+        node.Cond = walk(env, node.Cond, true)
+        node.Then = walk(env, node.Then, true)
         if node.Els != nil {
-            node.Els = walk(node.Els, true)
+            node.Els = walk(env, node.Els, true)
         }
         return node
     case ND_FOR:
-        node.Init = walk(node.Init, true)
-        node.Cond = walk(node.Cond, true)
-        node.Inc = walk(node.Inc, true)
-        node.Body = walk(node.Body, true)
+        node.Init = walk(env, node.Init, true)
+        node.Cond = walk(env, node.Cond, true)
+        node.Inc = walk(env, node.Inc, true)
+        node.Body = walk(env, node.Body, true)
         return node
     case '+', '-':
-        node.Lhs = walk(node.Lhs, true)
-        node.Rhs = walk(node.Rhs, true)
+        node.Lhs = walk(env, node.Lhs, true)
+        node.Rhs = walk(env, node.Rhs, true)
 
         if node.Rhs.Ty.Ty == PTR {
             swap(&node.Lhs, &node.Rhs)
@@ -125,34 +152,34 @@ func walk(node *Node, decay bool) *Node {
         node.Ty = node.Lhs.Ty
         return node
     case '=':
-        node.Lhs = walk(node.Lhs, false)
+        node.Lhs = walk(env, node.Lhs, false)
         if !(node.Lhs.Op == ND_LVAR || node.Lhs.Op == ND_DEREF) {
             Error(fmt.Sprintf("not an lvalue: %d (%s)", node.Op, node.Name))
         }
-        node.Rhs = walk(node.Rhs, true)
+        node.Rhs = walk(env, node.Rhs, true)
         node.Ty = node.Lhs.Ty
         return node
     case '*', '/', '<', ND_LOGAND, ND_LOGOR:
-        node.Lhs = walk(node.Lhs, true)
-        node.Rhs = walk(node.Rhs, true)
+        node.Lhs = walk(env, node.Lhs, true)
+        node.Rhs = walk(env, node.Rhs, true)
         node.Ty = node.Lhs.Ty
         return node
     case ND_ADDR:
-        node.Expr = walk(node.Expr, true)
+        node.Expr = walk(env, node.Expr, true)
         node.Ty = Ptr_of(node.Expr.Ty)
         return node
     case ND_DEREF:
-        node.Expr = walk(node.Expr, true)
+        node.Expr = walk(env, node.Expr, true)
         if node.Expr.Ty.Ty != PTR {
             Error("operand must be a pointer")
         }
         node.Ty = node.Expr.Ty.Ptr_of
         return node
     case ND_RETURN:
-        node.Expr = walk(node.Expr, true)
+        node.Expr = walk(env, node.Expr, true)
         return node
     case ND_SIZEOF:
-        expr := walk(node.Expr, false)
+        expr := walk(env, node.Expr, false)
 
         ret := new(Node)
         ret.Op = ND_NUM
@@ -161,23 +188,24 @@ func walk(node *Node, decay bool) *Node {
         return ret
     case ND_CALL:
         for i := 0; i < node.Args.Len; i++ {
-            node.Args.Data[i] = walk(node.Args.Data[i].(*Node), true)
+            node.Args.Data[i] = walk(env, node.Args.Data[i].(*Node), true)
         }
         node.Ty = &int_ty
         return node
     case ND_FUNC:
         for i := 0; i < node.Args.Len; i++ {
-            node.Args.Data[i] = walk(node.Args.Data[i].(*Node), true)
+            node.Args.Data[i] = walk(env, node.Args.Data[i].(*Node), true)
         }
-        node.Body = walk(node.Body, true)
+        node.Body = walk(env, node.Body, true)
         return node
     case ND_COMP_STMT:
+        newenv := new_env(env)
         for i := 0; i < node.Stmts.Len; i++ {
-            node.Stmts.Data[i] = walk(node.Stmts.Data[i].(*Node), true)
+            node.Stmts.Data[i] = walk(newenv, node.Stmts.Data[i].(*Node), true)
         }
         return node
     case ND_EXPR_STMT:
-        node.Expr = walk(node.Expr, true)
+        node.Expr = walk(env, node.Expr, true)
         return node
     default:
         Assert(false, "unknown node type")
@@ -192,12 +220,13 @@ func Sema(nodes *Vector) {
         node := nodes.Data[i].(*Node)
         Assert(node.Op == ND_FUNC, "node Op is not ND_FUNC")
 
-        // 各関数のローカル変数郡のためのmap
-        vars = make(map[string]interface{})
+        // // 各関数のローカル変数郡のためのmap
+        // vars = make(map[string]interface{})
+
         globals = New_vec()
         stacksize = 0
 
-        walk(node, true)
+        walk(new_env(nil), node, true)
         node.Stacksize = stacksize
         node.Globals = globals
     }
